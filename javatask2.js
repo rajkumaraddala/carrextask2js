357cdbacba79bfb29f98428a5707ef04f381a40c
rajkumaraddala <115741752+rajkumaraddala@users.noreply.github.com>
2022-11-07 00:50:30 +0530

    Add files via upload

---
+++ javatask2.js
@@ -0,0 +1,59 @@
+//TASK2Q-1
+var favoriteFood='pizza';
+console.log(favoriteFood);
+var numOfSlices=8;
+console.log('pizza');
+console.log(numOfSlices);
+
+//2-2task
+let changeMe=true;
+    changeMe='false';
+    console.log(changeMe);
+//2-3task
+const entree='Enchiladas';
+console.log(entree);
+
+
+/*here we are getting error because const variable does not change the value
+only let variable can be change the value */
+//2-4task
+let levelUp=10;
+let PowerLevel=9001;
+let multiplyMe=32;
+let quarterMe=1152;
+// mathematiclal assignments operations//
+ levelUp+=5;
+PowerLevel-=100;
+multiplyMe*=11;
+quarterMe/=20;
+console.log('The value of levelUp:',levelUp);
+console.log('The value of PowerLevel:',PowerLevel);
+console.log('The value of multiplyMe:',multiplyMe);
+console.log('The value of qarterMe:',quarterMe);
+//2-5task
+//here we use increment and decrement order
+let gainedDollar=3;
+let lostDollar=50;
+gainedDollar++;
+lostDollar--;
+console.log(gainedDollar);
+console.log(lostDollar);
+// 2-6task
+let favoriteAnimal="Dog";
+console.log('my favorite animal:ANIMAL');
+console.log('my favorite animal is a:','Dog');
+console.log('my favorite animal is:'+favoriteAnimal);
+//2-7task
+let myName='rajkumar addala';
+let myCity='vizag';
+console.log('My name is NAME.My city name is CITY');
+console.log('My name is:'+myName,'My city is:'+myCity);
+console.log('My name is rajkumar addala.My city is vizag');
+//2-8task
+//here used "typeof"  declare avariable type name
+let newVariable="Playing around with typeof";
+console.log(typeof newVariable);
+newVariable=20;
+console.log(typeof newVariable);
+newVariable=true;
+console.log(typeof newVariable);
